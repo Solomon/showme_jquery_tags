8e626904f2f4c955af9f7859a59f2b271eefcfe0
Solomon laptop <solomon.kahn@gmail.com>
2011-07-26 12:45:42 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ShowmeJqueryTags::Application.config.secret_token = '6133ab90f82d538d2eb120d38787815daa9d4c221b8e6d3e03e9549e0a080421160db958db8759454884177e82a0c10c8ba4b11111f527d31fbd73abc844e565'
